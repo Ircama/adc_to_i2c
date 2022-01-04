0b7abe6f31d59b5695c99186d6d7ef1526a41caf
Ircama <Ircama@users.noreply.github.com>
2022-01-05 00:59:04 +0100

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# adc_to_i2c
- Arduino based ADC to I2C gateway (to be used by Raspberry Pi)
+# Arduino-based ADC to I2C converter
+
+This Arduino code allows extending the Raspberry Pi with a simple 16-bit ADC support implemented through the integration of an Arduino-based ATTINY85/88 device interfaced via I2C (4-wire bus).
+
+Tested with an ATTINY88 device. It should also work with ATTINY85, Digispark, or other Arduino devices.
+
+While this code allows easy extensions and customizations (it is also fast to serve I2C readings), it is very slow in updating the 16-bit ADC samples. If additional performance is needed with 16-bit sampling, use a specific ADC to I2C hw converter instead, like the ADS1115.
+
+Features:
+
+- Read a configurable number and sequence of 10-bit hw analog ports (A0 to An)
+- Perform n-bit sw oversampling via [Arduino Oversampling And Decimation Library](https://github.com/regimantas/Oversampling) with averaging
+- Provide I2C slave interface to a Raspberry Pi
+- Include calculation of LM35 centigrade temperature analog sensors (to read appropriate temperature values, it is important to predefine the power supply voltage in the code; e.g.  3.3V or 5V, ref. `#define VREF`)
+- Decouple the queries performed by the Raspberry Pi with the rate of the ADC reading process (the latter taking seconds in case of several oversampled values without pre-scaling).
+
+Default configuration:
+
+- 16 bit oversampling with averaging of two samples (each oversampling reading returns the average of two oversampled values)
+- set the ADCSRA pre-scaler register to 64 instead of 128, to increase the sampling rate; notice anyway that for reading ambient temperatures (which generally have very slow variations), keeping the pre-scaler to 128 (safe value) is suggested to always ensure max read quality. Using a pre-scaler of 16 or 8 might still work in certain circumstances, highly improving the sampling rate, where the reduced quality is compensated by the 16-bit oversampling.
+- Preconfigured 4 ADC input ports (available with ATTINY88), which can be redefined via `#define NUM_ADC 4`
+- Used ports: 0, 1, 2, 3 (which can be redefined by changing `int adc_pins[] = {0, 1, 2, 3};`)
+- 3.3V power supply (which can be changed via `#define VREF 3.3`)
+- Use I2C 2A slave address (which can be redefined by changing `#define SLAVE_ADDRESS 0x2A`)
+- Upon each I2C query, the code returns a stream of 32 bytes corresponding to 8 numbers: 4 single-precision floating-point values (taking 4 bytes) and 4 unsigned long integers (also taking 4 bytes). The first 4 numbers are the LM35 temperatures in Celsius degrees, the other 4 numbers are the raw oversampled readings.
+- The 4 returned raw ADC values are integers between 0 and 65535 (16 bit values).
+
+# Raspberry Pi interface
+
+Installing pip3: `sudo apt-get install python3-pip`
+
+Installing smbus: `sudo apt-get install python3-smbus python3-dev`
+
+```python
+import struct
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+address = 0x2a
+data_array = bus.read_i2c_block_data(address, 0, 4 * 2 * 4) # 4 ADC ports, 2 numbers per each ADC port, 4 bytes per each "float" or "unsigned long" value 
+
+temperature = []
+for i in [data_array[x:x+4] for x in range(0, int(len(data_array)/2), 4)]:
+    temperature.append(struct.unpack('<f', bytes(i))[0])
+
+adc_value = []
+for i in [data_array[x:x+4] for x in range(int(len(data_array)/2), len(data_array), 4)]:
+    adc_value.append(struct.unpack('<L', bytes(i))[0])
+
+print(temperature, adc_value)
+
+```
+
+Note:
+
+The Arduino "Oversampling And Decimation Library" has been modified to manage 32-bit unsigned long return values, ref. https://github.com/regimantas/Oversampling/pull/2/files)
---
+++ adc_to_i2c/adc_to_i2c.ino
@@ -0,0 +1,131 @@
+/*
+ * Used Libraries:
+ * https://github.com/regimantas/Oversampling (modified for managing 32-bit unsigned long return vlues, ref. https://github.com/regimantas/Oversampling/pull/2/files)
+ * Default Wire library (I2C master/slave)
+ */
+
+#include <Oversampling.h>
+
+/*
+ * Oversampling(int adcbytes, int samplebytes, int Averaging)
+ * https://github.com/regimantas/Oversampling
+ * first parameter: adcbytes (always 10 on Attiny85 or Attiny88: min = 0, max = 1023)
+ * second parameter: samplebytes (target bytes to get from oversampling; could be a value between 11 and 24; typically 12 to double precision)
+ * third parameter: Averaging (it performs the sdampling more times, then computing the averaged value; 1 = single oversampling; 2 = average of two oversampling)
+ */
+Oversampling adc(10, 16, 2); // Adc Bytes 10 or 12, Oversampling Bytes 11-24, 2 is the averaging of more samples.
+
+/* 10 = Resolution of the ATTINY85/ATTINY88 ADC (= 10 bit)
+ * 16 = number of bits obtained with oversampling (e.g., from 10 bits to 16 bit)
+ * 2 = number of consecutive taken oversampled readings before returning the value, having calculated the average
+ *
+ * Here is how to map the first value (MULTIPLIER) of the divisor ("divisor) to the oversampling value.
+ * Oversampling -> first value of the divisor (Oversampling bytes = number)
+ * 16 = 64
+ * 15 = 32
+ * 14 = 16
+ * 13 = 8
+ * 12 = 4
+ * 11 = 2
+ */
+
+/*
+ * The following settings convert LM32 values to temperatures, basing on the VREF voltage
+ */
+//#define VREF 5.0 // Volt
+#define VREF 3.3 // Volt
+#define MULTIPLIER 64 // see above (depends on the number of bits defined for the oversampling)
+const float divisor = (float) MULTIPLIER / (VREF / 1024 * 100);
+
+/* The following two parameters define:
+ * - the number of ADCs to read
+ * - the mapping of the ADC number and its port
+ * NUM_ADC must be less than or equal to the number of elements in adc_pins.
+ * The higher NUM_ADC, the longer the time to refresh the ADC readings
+ */
+#define NUM_ADC 4
+int adc_pins[] = {0, 1, 2, 3};
+
+typedef struct {
+  float temp[NUM_ADC]; // temperature
+  unsigned long adc_value[NUM_ADC]; // raw value
+} type_temp;
+
+type_temp temp_set, out_set;
+
+/* The following parameter defines whether the ADC prescaler should be
+ * configured with a value other than the default (128). Minor is
+ * the value, higher the frequency (input clock frequency to the ADC)
+ * shorter the reading time (higher sample rate), greater the error in
+ * case of rapid fluctuations of the analog reading.
+ * With clock of 16MHz, you should not use a lower divider than 16.
+ */
+#define FASTADC 1
+
+/* I2C configuration and relative port on which the ADC readings are
+ * transmitted
+ */
+#include <Wire.h>
+#define SLAVE_ADDRESS 0x2A
+
+#define I2C_HARDWARE 1
+#define I2C_TIMEOUT 10
+#define I2C_MAXWAIT 10
+#define I2C_PULLUP 1
+#define I2C_FASTMODE 1
+#define SDA_PORT PORTC
+#define SDA_PIN 4 // = A4
+#define SCL_PORT PORTC
+#define SCL_PIN 5 // = A5
+
+/*-------------------------------------------------------------------------------------------------------*/
+void setup() {
+  // This setting modifies precision and performance
+  #if FASTADC // Default ADCSRA (FASTADC unset) is 128 (slow)
+    ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
+    
+    // uncomment as required
+    
+  //  ADCSRA |= bit (ADPS0);                               //   2  
+  //  ADCSRA |= bit (ADPS1);                               //   4  
+  //  ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8  
+  //  ADCSRA |= bit (ADPS2);                               //  16 
+  //  ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32 
+    ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64 
+  //  ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128 (default)
+  
+  // https://github.com/stylesuxx/Oversample#implementation
+  
+  #endif
+
+  /*
+  The DIDR (Data Input Disable Register) disconnects the digital inputs from
+  whichever ADC channels you are using. This is important for 2 reasons. First
+  off, an analog input will be floating all over the place, and causing the
+  digital input to constantly toggle high and low. This creates excessive noise
+  near the ADC, and burns extra power. Secondly, the digital input and associated
+  DIDR switch have a capacitance associated with them which will slow down your
+  input signal if you are sampling a highly resistive load.
+  */
+  for (int i=0; i< NUM_ADC; i++)
+    DIDR0 |= 1<<adc_pins[i];
+
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onRequest(sendData);
+} // setup
+
+void sendData() {
+  Wire.write((byte*)&out_set, sizeof(type_temp)); // stored bit per number=4; numbers = NUM_ADC; types = unsigned long, float = 32 bit = 4 bytes
+}
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+void loop() {
+  for (int i=0; i< NUM_ADC; i++) {
+    temp_set.adc_value[i] = adc.read(adc_pins[i]); // Oversampling and decimation
+    temp_set.temp[i] = (float) temp_set.adc_value[i] / divisor;
+  }
+  noInterrupts();
+  memcpy( (void*)&out_set, (void*)&temp_set, sizeof(type_temp) );
+  interrupts();
+}
